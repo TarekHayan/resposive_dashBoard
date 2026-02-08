d90b9cc05d13dc2211eb3f36c495530f840ba39a
Tarek Hayan <tarekhayan77@gmail.com>
2026-02-08 18:09:09 +0200

    Add : Show Active in Drawer items

--- lib/widgets/drawe_item.dart
+++ lib/widgets/drawe_item.dart
@@ -4,13 +4,46 @@ import 'package:responsive_dashboard/core/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/core/utils/app_styles.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key, required this.itemModel});
+  const DrawerItem({
+    super.key,
+    required this.itemModel,
+    required this.isActive,
+  });
   final DrawerItemModel itemModel;
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? ActiveDrawerItem(itemModel: itemModel)
+        : InActiveDrawerItem(itemModel: itemModel);
+  }
+}
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({super.key, required this.itemModel});
+
+  final DrawerItemModel itemModel;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(itemModel.image),
       title: Text(itemModel.title, style: AppStyles.styleMedium16),
+    );
+  }
+}
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({super.key, required this.itemModel});
+
+  final DrawerItemModel itemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(itemModel.image),
+      title: Text(itemModel.title, style: AppStyles.styleBold16),
+      trailing: Container(width: 3.27, color: const Color(0xff4EB7F2)),
     );
   }
 }
--- lib/widgets/drawer_items_list_view.dart
+++ lib/widgets/drawer_items_list_view.dart
@@ -3,16 +3,31 @@ import 'package:responsive_dashboard/core/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/core/utils/app_images.dart';
 import 'package:responsive_dashboard/widgets/drawe_item.dart';
 
-class DrawerItemsListView extends StatelessWidget {
+class DrawerItemsListView extends StatefulWidget {
   const DrawerItemsListView({super.key});
-  static const List<DrawerItemModel> items = [
-    DrawerItemModel(image: Assets.imagesDashboard, title: "Dashoard"),
-    DrawerItemModel(image: Assets.imagesMyTransaction, title: "My Transaction"),
-    DrawerItemModel(image: Assets.imagesStatistics, title: "Statistics"),
-    DrawerItemModel(image: Assets.imagesWalletAccount, title: "Wallet Account"),
-    DrawerItemModel(image: Assets.imagesMyInvestments, title: "My Investments"),
-  ];
 
+  @override
+  State<DrawerItemsListView> createState() => _DrawerItemsListViewState();
+}
+
+class _DrawerItemsListViewState extends State<DrawerItemsListView> {
+  int indexActive = 0;
+  final List<DrawerItemModel> items = [
+    const DrawerItemModel(image: Assets.imagesDashboard, title: "Dashoard"),
+    const DrawerItemModel(
+      image: Assets.imagesMyTransaction,
+      title: "My Transaction",
+    ),
+    const DrawerItemModel(image: Assets.imagesStatistics, title: "Statistics"),
+    const DrawerItemModel(
+      image: Assets.imagesWalletAccount,
+      title: "Wallet Account",
+    ),
+    const DrawerItemModel(
+      image: Assets.imagesMyInvestments,
+      title: "My Investments",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -20,9 +35,21 @@ class DrawerItemsListView extends StatelessWidget {
       physics: const NeverScrollableScrollPhysics(),
       itemCount: 5,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: DrawerItem(itemModel: items[index]),
+        return GestureDetector(
+          onTap: () {
+            if (indexActive != index) {
+              setState(() {
+                indexActive = index;
+              });
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: DrawerItem(
+              itemModel: items[index],
+              isActive: indexActive == index,
+            ),
+          ),
         );
       },
     );
